e4fdae0b529049a0fa0bf4ef0e1e99846f3f193c
arlindabeqiraj <arlinda.beqiraj1@student.uni-pr.edu>
2024-05-19 21:12:18 +0200

    Vigenere Cipher

--- src/main/java/Projekti3DS/Projekti3.java
+++ src/main/java/Projekti3DS/Projekti3.java
@@ -1,4 +1,44 @@
 package Projekti3DS;
 
-public class Projekti3 {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Projekti3 extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
+class VigenereCipher {
+    public static String encrypt(String text, String key) {
+        StringBuilder result = new StringBuilder();
+        key = key.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (Character.isLetter(c)) {
+                char base = Character.isLowerCase(c) ? 'a' : 'A';
+                result.append((char) ((c - base + (key.charAt(j % key.length()) - 'A')) % 26 + base));
+                j++;
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+    public static String decrypt(String text, String key) {
+        StringBuilder result = new StringBuilder();
+        key = key.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (Character.isLetter(c)) {
+                char base = Character.isLowerCase(c) ? 'a' : 'A';
+                result.append((char) ((c - base - (key.charAt(j % key.length()) - 'A') + 26) % 26 + base));
+                j++;
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
 }
