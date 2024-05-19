3d940aa12d186a99174488e5823471460ed3fed5
Argjend <argjend.nimanaj@student.uni-pr.edu>
2024-05-19 21:56:31 +0200

    CaesarCipher

--- src/main/java/Projekti3DS/Projekti3.java
+++ src/main/java/Projekti3DS/Projekti3.java
@@ -9,6 +9,28 @@ public class Projekti3 extends Application {
 
     }
 }
+
+class CaesarCipher {
+    public static String encrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < text.length(); i++){
+            char c = text.charAt(i);
+            if(Character.isLetter(c)){
+                char base = Character.isLowerCase(c) ? 'a' : 'A';
+                result.append((char) ((c - base + shift) % 26 + base));
+            }
+            else{
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+    public static String decrypt(String text, int shift) {
+        return encrypt(text, 26 - shift);
+    }
+}
+
 class VigenereCipher {
     public static String encrypt(String text, String key) {
         StringBuilder result = new StringBuilder();
